68afd9120d9eae59f730ec8fb6ebb9116357f478
mellowjoeys <mellowjoey@gmail.com>
2019-10-11 16:45:03 -0500

    added User authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,4 +8,18 @@ class Api::UsersController < ApplicationController
     @user = User.find(params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    user = User.new(
+      username: params[:username],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      zip_code: params[:zip_code]
+      )
+    if user.save
+      render json: {message: "User created successfuly"}, status: :created
+    else 
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/users' => 'users#index'
+    post '/users' => 'users#create'
     get '/users/:id' => 'users#show'
 
     get '/goods' => 'goods#index'
@@ -8,5 +9,8 @@ Rails.application.routes.draw do
 
     get '/photos' => 'photos#index'
     get '/photos/:id' => 'photos#show'
+
+    post '/sessions' => 'sessions#create'
+    
   end
 end
