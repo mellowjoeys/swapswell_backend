7441db38fb2ad7c5571fee465d1acdd1fc4a50b8
brittani <britt.sentiere@gmail.com>
2019-10-22 19:33:47 -0500

    added method to photos controller

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -21,6 +21,24 @@ class Api::PhotosController < ApplicationController
     render 'show.json.jb'
   end
 
+  def index_minus_own
+    goods_minus_own = []
+    
+    goods = Good.all
+    goods.each do |good|
+      if good.user_id != current_user.id
+        goods_minus_own << good
+      end 
+    end
+    @photos = []
+    goods_minus_own.each do |good|
+      good.photos.each do |photo|
+        @photos << photo
+      end 
+    end
+    render 'index.json.jb'
+  end
+
   # def update
   #   @photo = Photo.find(params[:id])
   #   @photo.image_url = params[:image_url] || @photo.image_url
